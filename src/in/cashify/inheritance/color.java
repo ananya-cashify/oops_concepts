ed7ae16b8452788202baef91306accb57e6be4f6
Cashify-1002138 <ananya.t@cashify.in>
2021-05-03 11:24:42 +0530

    Added inheritance example

---
+++ src/in/cashify/inheritance/color.java
@@ -0,0 +1,21 @@
+package in.cashify.inheritance;
+
+public  abstract class color {
+
+    public color (String colorname)
+    {
+        System.out.println(colorname);
+    }
+    public  abstract void black();
+
+    public void  pink(){
+        System.out.println("I am Pink");
+    }
+
+    public void red(){
+        System.out.println("I am red");
+
+    }
+
+
+}
---
+++ src/in/cashify/inheritance/design.java
@@ -0,0 +1,33 @@
+package in.cashify.inheritance;
+
+public class design extends color{
+
+    public design(String colorname) {
+        super(colorname);
+    }
+
+    @Override
+    public void black() {
+        System.out.println("I am Black");
+
+    }
+
+    @Override
+    public void pink() {
+                System.out.println("I am Pink");
+
+
+    }
+    @Override
+    public void red(){
+                System.out.println("I am Red");
+
+
+    }
+    public static void main(String[] args){
+        design d = new design ("white");
+        d.black();
+        d.pink();
+        d.red();
+    }
+}
