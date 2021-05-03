0003ab1346455041e92cfe10d8b36ae88b441eaa
Cashify-1002138 <ananya.t@cashify.in>
2021-05-03 11:48:31 +0530

    added 1 more class to inheritance

---
+++ src/in/cashify/inheritance/house.java
@@ -0,0 +1,17 @@
+package in.cashify.inheritance;
+
+public class house extends color {
+
+
+    public house(String colorname) {
+        super(colorname);
+    }
+
+    @Override
+    public void black() {
+        System.out.println("I am Black");
+    }
+    public static void main (String[] args){
+        house h = new house ("white");
+    }
+}
