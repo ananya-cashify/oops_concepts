71a9625012a393b23c0c75e45b638452df0b08ee
Cashify-1002138 <ananya.t@cashify.in>
2021-05-11 14:06:12 +0530

    added Abstraction

--- src/in/cashify/polymorphism/Vehicle.java
+++ src/in/cashify/polymorphism/Vehicle.java
@@ -3,12 +3,17 @@ package in.cashify.polymorphism;
 public class Vehicle {
     void run(){System.out.println("running");}
 }
-class Splendor extends Vehicle{
-    void run(){System.out.println("running safely with 60km");}
+
+interface Honda1{
+    void run ();
+}
+
+class Splendor extends Vehicle implements Honda1 {
+    public void run() {
+        System.out.println("running safely with 60km"); }
 
     public static void main(String args[]){
         Vehicle b = new Splendor();//upcasting
         b.run();
     }
 }
-
