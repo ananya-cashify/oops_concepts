1d26eab7b86f18532424fee227f7deb0563d5457
Cashify-1002138 <ananya.t@cashify.in>
2021-05-14 17:43:31 +0530

    added enum example

---
+++ src/in/cashify/enumExample/DataType.java
@@ -0,0 +1,12 @@
+package in.cashify.enumExample;
+
+public class DataType {
+
+    byte i = 109;
+    short j = 10900;
+    int  k = 6731687;
+    long l = 7625173;
+    double  m = 781872136;
+    float n = 26737823;
+
+}
